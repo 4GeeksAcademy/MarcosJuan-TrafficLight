9427018b077e3a01fd6bfeda1df0e08c28a54e7e
Marcos Juan Martos Prados <martospradosmarcos@gmail.com>
2024-12-07 16:02:31 +0000

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Home } from "./component/home.jsx";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
